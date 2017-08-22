79b73cb8c405af917fe925a783d51131f03757e6
Shenshen Han <hanaldo520124@gmail.com>
2017-08-22 17:21:51 -0400

    New language data format.

---
+++ static_data/language_list.js
@@ -0,0 +1,26 @@
+const supportedLanguages = [
+    {
+        code: "en",
+        name: "English"
+    },
+    {
+        code: "zh-cn",
+        name: "中文（简体）"
+    },
+    {
+        code: "zh-hk",
+        name: "中文（繁體）"
+    },
+//    {
+//        code: "de",
+//        name: "Deutsch"
+//    },
+//    {
+//        code: "es",
+//        name: "Español"
+//    },
+    {
+        code: "ja",
+        name: "日本語"
+    }
+];
