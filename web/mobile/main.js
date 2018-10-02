e53cae8f8fcf1d1433a1f61069a87a9ebed8688b
Shenshen Han <hanaldo520124@gmail.com>
2018-10-02 12:21:43 -0400

    Disable devTool as default.

--- web/mobile/main.js
+++ web/mobile/main.js
@@ -13,7 +13,7 @@ function createWindow() {
   win.loadFile("index.html");
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   win.on("closed", () => {
     win = null
